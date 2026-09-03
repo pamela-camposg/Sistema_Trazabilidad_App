e0ead9628e32c5de3d3b4ca1577663650be8ecab
pamela-camposg <pacamposgonzalez@gmail.com>
2026-09-03 13:15:43 -0400

    Update VERSION from 16 to 18

--- worker.js
+++ worker.js
@@ -24,7 +24,7 @@ const WHEELS = [
   "python/wheels/openpyxl-3.1.5-py2.py3-none-any.whl",
 ];
 
-const VERSION = "16";
+const VERSION = "18";
 
 let pyodide = null;
 let base = self.location.href;   // la reemplaza el mensaje "iniciar"
