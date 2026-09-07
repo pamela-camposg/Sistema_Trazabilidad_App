bfdea766a66da4356b653f41262bcd1f68c07800
pamela-camposg <pacamposgonzalez@gmail.com>
2026-09-07 13:26:39 -0300

    Update consolidar.py

--- python/sur/consolidar.py
+++ python/sur/consolidar.py
@@ -586,8 +586,6 @@ def cargar_lookups(rutas, p):
 # Verlas distinto al abrir el archivo evita tratarlas como dato de terreno.
 COLUMNAS_DESTACADAS = {
     "Origen ID",
-    "Comuna Destino",
-    "Movimiento",
     "Movimiento interempresa",
     "Destino inferido",
 }
